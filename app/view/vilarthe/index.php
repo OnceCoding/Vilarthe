dcb01bafded3e86f46e6462459ff6579262a0a87
Jose Suarez <jose95sp@outlook.com>
2018-01-13 11:42:14 -0500

    agrega pagina principal vilarthe, falta responsividad

--- app/view/vilarthe/index.php
+++ app/view/vilarthe/index.php
@@ -1,16 +1,34 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Vilathe</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100" rel="stylesheet">
+    <link rel="stylesheet" href="public/vilarthePublic/css/styles.css">
 </head>
 <body>
-    <h1>Pagina principal de vilarthe</h1>
-    <a href="lazos">
-        <button>LAZOS</button>
-    </a>
+    <div class="brand">
+        <div class="">
+            <img class="imageBrand" src="public/vilarthePublic/images/logoFondoOscuro.png" alt="">
+        </div>
+        <div class="setCategories">
+            <ul class="categories">
+                <li class="divider"> </li>
+                <li class="item active"><a href="lazos/">Lazos</a></li>
+                <li class="divider"></li>
+                <!--li class="item ">Armani</li>
+                <li class="divider"></li>
+                <li class="item ">Nike</li>
+                <li class="divider"></li-->
+            </ul>
+        </div>
+    </div>
+
+    <div class="slider">
+        <div class="image">
+            <!--img src="images/baby1.jpg" alt=""-->
+        </div>
+    </div>
+
 </body>
 </html>
