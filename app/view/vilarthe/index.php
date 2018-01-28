531443df5135105ec9a27def06a68f8b7565367f
Jose Suarez <jose95sp@outlook.com>
2018-01-27 19:42:28 -0500

    descripcion de vilarthe

--- app/view/vilarthe/index.php
+++ app/view/vilarthe/index.php
@@ -16,7 +16,12 @@
     <meta property="og:image:height" content="225" />
     <meta property="og:image:alt" content="Logo de Vilarthe" />
 
-    <meta property="og:description" content="Vilarthe es una empresa que se dedica a la venta de ropa con algodón organico">
+    <meta property="og:description" content="Vilarthe en una marca de ropa y accesorios creada en el departamento de Lambayeque
+        en el norte del Perú con el objetivo de hacerle llegar lo mejor de nuestras fibras elaboradas con el
+        mejor algodón de la región legado de nuestro pasado milenario.
+        Vilarthe ha creado la línea LAZOS BABY CLOTHING diseñada pensando en proteger la sensible y
+         delicada piel de los bebés, están elaborados con algodón peruano (pima o nativo),
+         Orgánicos, y teñidos 100% con tintes naturales, los accesorios son antialérgicos libres de níquel. ">
 
     <meta name="author" content="Vilarthe">
     <meta name="classification" content="all">
@@ -63,7 +68,6 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
